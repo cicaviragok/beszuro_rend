418f9f110b1bb3bbbd39d69fd11b45e07966a4b0
Ábrahám Leila (SZF_2022_2024) <abraham.leila@diak.szamalk-szalezi.hu>
2023-05-03 13:22:42 +0200

    rendezes

--- src/beszuras_rendezees/Beszuras_rendezees.java
+++ src/beszuras_rendezees/Beszuras_rendezees.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package beszuras_rendezees;
 
-/**
- *
- * @author FranyóKrisztián(SZF_
- */
 public class Beszuras_rendezees {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int lista[] = {69, 19, 6, 420};
+        for (int i = 0; i < lista.length; i++) {
+            System.out.print(lista[i]+" ");
+        }
+        System.out.println("");
+        
+        int j;
+        for (int i = 1; i < lista.length ;i++) {
+            int x = lista[i];
+            j = i-1;
+            while (j >= 0 && lista[j] > x) {
+                lista[j+1] = lista[j];
+                j--;
+            }
+            lista[j+1] = x;
+        }
+        for (int i = 0; i < lista.length; i++) {
+            System.out.print(lista[i]+" ");
+        }
+            
+        
+        
+
     }
-    
+
 }
